35c7562112114334c7867937d049bdd984ebfdeb
Roi <68617740+Roizor@users.noreply.github.com>
2023-03-14 16:48:05 -0500

    bhnfgnh

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "ignorePatterns": ["*.js"],
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+}
