32fe3f00b88d1e5624dc22cff73b1c1bdc28c71f
Roi <68617740+Roizor@users.noreply.github.com>
2023-03-28 21:48:47 -0500

    sdfg

---
+++ src/errors/WPPErrorDict.ts
@@ -0,0 +1,9 @@
+export enum WPPErrorDictionary {
+    "WPT-1mY" = "No title text was included with W++.",
+    "WPI-2FY" = "W++ is already injected. Please refresh and then reinject.",
+    "WPF-3mY" = "W++ failed to inject it's CSS. Try reinjecting.",
+    "WPF-4MY" = "W++ failed to inject it's JS. Reinject W++.",
+    "WPF-5FN" = "W++ failed to inject. Try reinjecting.",
+    "WPA-6mN" = "W++ found W++ CSS already. Attempting to override.",
+    "WPA-7mN" = "W++ found W++ JS already. Attempting to override."
+}
