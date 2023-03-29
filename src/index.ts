837feb58f89d5cdb6d9ffedccbe86a254bf13e80
Roi <68617740+Roizor@users.noreply.github.com>
2023-03-28 21:44:50 -0500

    coolio

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,6 @@ import main_theme from './themes/main_theme.wpp'
 import logger from "./mods/logger"
 import injector from "./inject/main"
 
-new logger.Logger(logger.LogLevels.init).log('Checking for previous injection, and removing.')
-document.getElementById('wpp-script') ? document.getElementById('wpp-script').remove() : 0;
-document.getElementById('wpp-style') ? document.getElementById('wpp-style').remove() : 0;
-
 try {
     injector.aio(main_theme, main_theme, 'W++')
 } catch(err) {
