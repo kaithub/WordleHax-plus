f6a21071abdddeb5b88b3eb04957f6d1de6020cb
Roi <68617740+Roizor@users.noreply.github.com>
2023-03-28 20:03:43 -0500

    message

--- src/inject/main.ts
+++ src/inject/main.ts
@@ -6,6 +6,7 @@ import trackerRemoval from "../mods/trackerRemoval";
 import { WPPGlobal } from "../WPPGlobal";
 import cssInjector from "./cssInjector";
 import jsInjector from "./jsInjector";
+import welcome from "./welcome";
 
 export default {
     css: cssInjector,
@@ -34,15 +35,7 @@ export default {
         document.getElementById('help-button').onclick = () => {
             setTimeout(() => {
                 getFElement('Modal-module_heading__u2uxI').innerHTML = 'Welcome to W++!'
-                getFElement('Help-module_subheading__mbRG9').innerHTML = `
-                <p>The new and easiest way to mod & theme Wordle.</p>
-                <p>You can edit this message however you want in <span style='background-color:#c4ce37'>src/inject/main.ts</span> then RRR (rebuild, refresh, reinject).</p>
-                <p>To make it easier, here's a refresh button. <button class='Key-module_key__kchQI' type='button' onclick='window.location.replace(window.location.href)'>Go</button></p>
-                <h1 class='Modal-module_heading__u2uxI Modal-module_newHeading__Ah45w'>Wanna edit the theme?</h1>
-                <p>Editing the theme is as easy as changing a few lines of CSS in <span style='background-color:#c4ce37'>src/themes/main_theme.wpp.ts</span>
-                <h1 class='Modal-module_heading__u2uxI Modal-module_newHeading__Ah45w'>Also..</h1>
-                <p>The RAL (Remove Absent Letters) challenge is enabled by default. Turn it off in <span style='background-color:#c4ce37'>src/themes/main_theme.wpp.ts</span>!
-                <h1 class='Modal-module_heading__u2uxI Modal-module_newHeading__Ah45w'>Have fun!</h1>`
+                getFElement('Help-module_subheading__mbRG9').innerHTML = welcome
                 getFElement('Help-module_examples__W3VXL').remove()
                 getFElement('Help-module_statsLogin__HkQec').remove()
                 getFElement('Help-module_instructions__uXsG6').remove()
---
+++ src/inject/welcome.ts
@@ -0,0 +1,9 @@
+export default `
+<p>The new and easiest way to mod & theme Wordle.</p>
+<p>You can edit this message however you want in <span style='background-color:#c4ce37'>src/inject/welcome.ts</span> then RRR (rebuild, refresh, reinject).</p>
+<p>To make it easier, here's a refresh button. <button class='Key-module_key__kchQI' type='button' onclick='window.location.replace(window.location.href)'>Go</button></p>
+<h1 class='Modal-module_heading__u2uxI Modal-module_newHeading__Ah45w'>Wanna edit the theme?</h1>
+<p>Editing the theme is as easy as changing a few lines of CSS in <span style='background-color:#c4ce37'>src/themes/main_theme.wpp.ts</span>
+<h1 class='Modal-module_heading__u2uxI Modal-module_newHeading__Ah45w'>Also..</h1>
+<p>The RAL (Remove Absent Letters) challenge is enabled by default. Turn it off in <span style='background-color:#c4ce37'>src/themes/main_theme.wpp.ts</span>!
+<h1 class='Modal-module_heading__u2uxI Modal-module_newHeading__Ah45w'>Have fun!</h1>`
