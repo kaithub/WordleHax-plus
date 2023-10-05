b698db99d8f2c26af096fae98ed9f73881aec996
kaithub <140258324+kaithub@users.noreply.github.com>
2023-10-06 00:20:50 +0200

    i forgor 302 errors suck :(

--- docs/whp-build.js
+++ docs/whp-build.js
@@ -1,6 +1,6 @@
 javascript:(function() {
     var xhr = new XMLHttpRequest;
-    xhr.open('GET', 'https://github.com/kaithub/WordleHax-plus/raw/master/build/WordleHaxPlus.js', true);
+    xhr.open('GET', 'https://raw.githubusercontent.com/kaithub/WordleHax-plus/master/build/WordleHaxPlus.js', true);
     xhr.onload = function() {
         if (xhr.status === 200) {
             var response = xhr.responseText;
