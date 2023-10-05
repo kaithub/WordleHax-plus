2683573d72af7d783b1cfe06f8c0830de7ee9e77
kaithub <140258324+kaithub@users.noreply.github.com>
2023-10-06 01:04:36 +0200

    would this work?

--- docs/whp-build.js
+++ docs/whp-build.js
@@ -1,11 +1,21 @@
 javascript:(function() {
+    import * as zip from "https://deno.land/x/zipjs/index.js";
     var xhr = new XMLHttpRequest;
-    xhr.open('GET', 'https://raw.githubusercontent.com/kaithub/WordleHax-plus/master/build/WordleHaxPlus.js', true);
+    xhr.open('GET', 'https://nightly.link/kaithub/WordleHax-plus/workflows/build/master/WordleHaxPlus.zip', true);
     xhr.onload = function() {
         if (xhr.status === 200) {
             var response = xhr.responseText;
-            console.log("sheesh, autorunning!");
-            eval(response)
+            zip.createReader(new zip.BlobReader(zipData), function(reader) {
+                reader.getEntries(function(entries) {
+                  if (entries.length) {
+                    var entry = entries[0];
+                    entry.getData(new zip.TextWriter(), function(text) {
+                        console.log("sheesh, autorunning!");
+                        eval(text);
+                    });
+                  }
+                });
+            });
         } else {
             console.log('Error: ' + xhr.status);
         }
