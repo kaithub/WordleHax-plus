0ed29ea3a959e60e531dcdd418f12b0940210d61
kaithub <140258324+kaithub@users.noreply.github.com>
2023-10-06 00:16:55 +0200

    proof im retarded as shit:

--- docs/whp-build.js
+++ docs/whp-build.js
@@ -1,24 +1,14 @@
 javascript:(function() {
-    // Create a new XMLHttpRequest
-    var xhr = new XMLHttpRequest
-
-    // Set the HTTP method and URL
+    var xhr = new XMLHttpRequest;
     xhr.open('GET', 'https://github.com/kaithub/WordleHax-plus/raw/master/build/WordlePlusPlus.js', true);
-
-    // Set the callback function to handle the response
     xhr.onload = function() {
-    if (xhr.status === 200) {
-        // The request was successful
-        var response = xhr.responseText;
-        // Run the content here
-        console.log("sheesh, autorunning!");
-        eval(response)
-    } else {
-        // The request failed
-        console.log('Error: ' + xhr.status);
-    }
+        if (xhr.status === 200) {
+            var response = xhr.responseText;
+            console.log("sheesh, autorunning!");
+            eval(response)
+        } else {
+            console.log('Error: ' + xhr.status);
+        }
     };
-
-    // Send the request
     xhr.send();
 })();
