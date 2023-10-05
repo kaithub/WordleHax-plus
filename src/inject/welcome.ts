8ba741a852a7360d17ee411a0cd6d20d1dc8d502
kaithub <140258324+kaithub@users.noreply.github.com>
2023-10-06 00:22:48 +0200

    hope this works lmao

--- src/inject/welcome.ts
+++ src/inject/welcome.ts
@@ -4,7 +4,7 @@ export default `
 <p>The new and easiest way to mod & theme Wordle.</p>
 <p>You can edit this message however you want in <span style='background-color:#c4ce37'>src/inject/welcome.ts</span> then RRR (rebuild, refresh, reinject).</p>
 <p>To make it easier, here's a refresh button. <button class='Key-module_key__kchQI' type='button' onclick='alert('Warning: You will need to reinject WH+.'); window.location.replace(window.location.href)'>Go</button></p>
-<p>Click this button to restart the wordle. (To try the wordle again)<button class='Key-module_key__kchQJ' type='button' onclick='localStorage.removeItem("nyt-wordle-moogle/ANON");'>Go</button></p>
+<p>Click this button to restart the wordle. (To try the wordle again)<button class='Key-module_key__kchQI' type='button' onclick='localStorage.removeItem("nyt-wordle-moogle/ANON");'>Go</button></p>
 <h1 class='Modal-module_heading__u2uxI Modal-module_newHeading__Ah45w'>Wanna edit the theme?</h1>
 <p>Editing the theme is as easy as changing a few lines of CSS in <span style='background-color:#c4ce37'>src/themes/main_theme.whp.ts</span>
 <h1 class='Modal-module_heading__u2uxI Modal-module_newHeading__Ah45w'>Also..</h1>
