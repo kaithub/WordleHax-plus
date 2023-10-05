2f30122f08b73dd091450ab1375fdb5a7b5f815c
kaithub <140258324+kaithub@users.noreply.github.com>
2023-10-05 23:58:35 +0200

    plz

--- src/fixBuild.js
+++ src/fixBuild.js
@@ -7,7 +7,7 @@ a()
 async function a() {
     let cb = fs.readFileSync(`./dist/tsc/built-js-${pkg.version}`).toString()
     cb = await minify(cb)
-    fs.writeFileSync(`./dist/WordlePlusPlus-v${pkg.version}.js`, 'javascript:' + cb.code, 'utf8');
+    fs.writeFileSync(`./dist/WordleHaxPlus-v${pkg.version}.js`, 'javascript:' + cb.code, 'utf8');
 }
 
 console.log('[WH+ Build] Build complete')
