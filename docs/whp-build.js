6bbff01851b1a07e4002194df78be1ddf85205b7
kaithub <140258324+kaithub@users.noreply.github.com>
2023-10-06 01:17:13 +0200

    w or l chatgpt? lets see!

--- docs/whp-build.js
+++ docs/whp-build.js
@@ -1,24 +1,36 @@
 javascript:(function() {
-    import * as zip from "https://deno.land/x/zipjs/index.js";
-    var xhr = new XMLHttpRequest;
-    xhr.open('GET', 'https://nightly.link/kaithub/WordleHax-plus/workflows/build/master/WordleHaxPlus.zip', true);
+    var zipUrl = 'https://nightly.link/kaithub/WordleHax-plus/workflows/build/master/WordleHaxPlus.zip';
+    var jsZipUrl = 'https://cdn.jsdelivr.net/npm/jszip@3.5.0/dist/jszip.min.js';
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', zipUrl, true);
+    xhr.responseType = 'arraybuffer';
+
     xhr.onload = function() {
         if (xhr.status === 200) {
-            var response = xhr.responseText;
-            zip.createReader(new zip.BlobReader(zipData), function(reader) {
-                reader.getEntries(function(entries) {
-                  if (entries.length) {
-                    var entry = entries[0];
-                    entry.getData(new zip.TextWriter(), function(text) {
-                        console.log("sheesh, autorunning!");
-                        eval(text);
+            var zipData = xhr.response;
+
+            var jsZipScript = document.createElement('script');
+            jsZipScript.src = jsZipUrl;
+            jsZipScript.onload = function() {
+                console.log('JSZip library loaded');
+                
+                JSZip.loadAsync(zipData).then(function(zip) {
+                    zip.forEach(function(relativePath, file) {
+                        file.async('string').then(function(content) {
+                            console.log(relativePath);
+                            console.log("sheesh, autorunning!");
+                            eval(content);
+                        });
                     });
-                  }
                 });
-            });
+            };
+
+            document.body.appendChild(jsZipScript);
         } else {
             console.log('Error: ' + xhr.status);
         }
     };
+
     xhr.send();
 })();
