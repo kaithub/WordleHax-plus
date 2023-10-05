bf1812ed739badcecb30d50d278c3ee2a322bf88
kaithub <140258324+kaithub@users.noreply.github.com>
2023-10-06 00:38:40 +0200

    last update for now(i hope)

--- src/WHPGlobal.ts
+++ src/WHPGlobal.ts
@@ -1,6 +1,6 @@
 export enum WHPGlobal {
     IsSilent = "",
-    Version = "1.2.2",
+    Version = "1.2.3",
     Header = 'AppHeader-module_title__EQr6V',
     BelowKeyboard = 'App-module_gameContainer__K_CBh',
     Tile = 'Tile-module_tile__UWEHN'
