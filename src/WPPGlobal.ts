f68bff0d9b2bedcfcb7726d8fbb73bc44d5ac5c3
kaithub <140258324+kaithub@users.noreply.github.com>
2023-10-05 23:48:31 +0200

    pls?

--- src/WPPGlobal.ts
+++ src/WPPGlobal.ts
@@ -1,6 +1,6 @@
 export enum WPPGlobal {
     IsSilent = "",
-    Version = "1.2.0",
+    Version = "1.2.1",
     Header = 'AppHeader-module_title__EQr6V',
     BelowKeyboard = 'App-module_gameContainer__K_CBh',
     Tile = 'Tile-module_tile__UWEHN'
