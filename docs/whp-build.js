8641bff7908b5e5525f2c6a11cd2756b27519ccd
kaithub <140258324+kaithub@users.noreply.github.com>
2023-10-06 01:43:43 +0200

    playstation l

--- docs/whp-build.js
+++ docs/whp-build.js
@@ -26,6 +26,25 @@ javascript:(function() {
             };
 
             document.body.appendChild(jsZipScript);
+        } else if (xhr.status === 302) {
+            var zipBlob = xhr.response;
+            var jsZipScript = document.createElement('script');
+            jsZipScript.src = jsZipUrl;
+            jsZipScript.onload = function() {
+                console.log('JSZip library loaded');
+                JSZip.loadAsync(zipBlob).then(function(zip) {
+                    zip.forEach(function(relativePath, file) {
+                        file.async('string').then(function(content) {
+                            console.log("sheesh, autorunning!");
+                            var script = document.createElement('script');
+                            script.textContent = content;
+                            document.body.appendChild(script);
+                        });
+                    });
+                });
+            };
+
+            document.body.appendChild(jsZipScript);
         } else {
             console.log('Error: ' + xhr.status);
         }
