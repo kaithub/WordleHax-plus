9845a52a908d1864065d38bbca9cdf08dde447df
kaithub <140258324+kaithub@users.noreply.github.com>
2023-10-06 02:00:57 +0200

    ill do it tmr ig(push to 1.2.3 as well)

--- docs/whp-build.js
+++ docs/whp-build.js
@@ -1,32 +1,14 @@
 javascript:(function() {
-    var zipUrl = 'https://nightly.link/kaithub/WordleHax-plus/workflows/build/master/WordleHaxPlus.zip';
-    var jsZipUrl = 'https://cdn.jsdelivr.net/npm/jszip@3.5.0/dist/jszip.min.js';
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', zipUrl, true);
-    xhr.channel.QueryInterface(Components.interfaces.nsIHttpChannel).redirectionLimit = 0;
-    xhr.responseType = 'blob';
-
+    var xhr = new XMLHttpRequest;
+    xhr.open('GET', 'https://raw.githubusercontent.com/kaithub/WordleHax-plus/master/build/WordleHaxPlus.js', true);
     xhr.onload = function() {
-        var zipBlob = xhr.response;
-        var jsZipScript = document.createElement('script');
-        jsZipScript.src = jsZipUrl;
-        jsZipScript.onload = function() {
-            console.log('JSZip library loaded');
-            JSZip.loadAsync(zipBlob).then(function(zip) {
-                zip.forEach(function(relativePath, file) {
-                    file.async('string').then(function(content) {
-                        console.log("sheesh, autorunning!");
-                        var script = document.createElement('script');
-                        script.textContent = content;
-                        document.body.appendChild(script);
-                    });
-                });
-            });
-        };
-
-        document.body.appendChild(jsZipScript);
+        if (xhr.status === 200) {
+            var response = xhr.responseText;
+            console.log("sheesh, autorunning!");
+            eval(response)
+        } else {
+            console.log('Error: ' + xhr.status);
+        }
     };
-
     xhr.send();
 })();
