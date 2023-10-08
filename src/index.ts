fab84acb5920e5b62d5b62069917c9c7b5cedbe1
kaithub <140258324+kaithub@users.noreply.github.com>
2023-10-08 16:44:25 +0200

    error catching

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,9 @@ import main_theme from './themes/main_theme.whp'
 import logger from "./mods/logger"
 import injector from "./inject/main"
 
-try {
+// try {
     injector.aio(main_theme, main_theme, 'WH+')
-} catch(err) {
+    new logger.Logger(logger.LogLevels.success).log('Injected WH+')
+/* } catch(err) {
     new logger.Logger(logger.LogLevels.error).log('ERR WPF-5fN: '+err)
-}
-
-new logger.Logger(logger.LogLevels.success).log('Injected WH+')
+} */
